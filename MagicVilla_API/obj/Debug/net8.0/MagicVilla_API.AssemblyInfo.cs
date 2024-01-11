f71420f2501571470722721e21f76afe2641b8b1
Alan Mundler <alan.mundler@gmail.com>
2024-01-11 15:44:47 -0300

    DTO y Automapper

--- MagicVilla_API/obj/Debug/net8.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla_API/obj/Debug/net8.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30b198bfbadd8d210e150d484487db56898081c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
